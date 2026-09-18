d5733c9d18c3857e89e55d39a0dcd83f536f7a7e
DavidArten5 <davidarten5@gmail.com>
2026-09-18 12:51:37 -0300

    agora o relogio funciona

--- alarme projeto.py
+++ alarme projeto.py
@@ -1,26 +1,18 @@
 import time
 state = 0
-config_alarme = '1748'
-aciona_buzzer= False
+config_alarme = '1214'
 def obter_hora_atual_string():
     tempo_local = time.localtime()
     hora = tempo_local.tm_hour
     minuto = tempo_local.tm_min
-    return f"{hora:02d}{minuto:02d}"
-    hora1 = str(hora) 
-    minuto1 = str(minuto)
+    return f"{hora:02d}:{minuto:02d}"
 
-print(hora1 + ":"+ minuto1)
-if aciona_buzzer ==True:
-    print("buzer bip bip bip")
 
+print(obter_hora_atual_string()) 
+ print("buzer bip bip bip")
 print("alarme configurado para"+config_alarme)
 
 while True:
     if state == 0:
-        hora_ataual= obter_hora_atual_string()
-        if hora_ataual == config_alarme:
-            aciona_buzzer= True
-    else:
-        aciona_buzzer= False
-def sleep()
+        print(obter_hora_atual_string())              
+time.sleep(360)   
