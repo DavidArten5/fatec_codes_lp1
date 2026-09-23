13e998c90498eb6bee70a6eb33f9d6482b895603
DavidArten5 <davidarten5@gmail.com>
2026-09-23 10:29:18 -0300

    Add files via upload
    sleep implementado com sucesso

--- alarme projeto.py
+++ alarme projeto.py
@@ -1,6 +1,5 @@
 import time
 state = 0
-config_alarme = '1214'
 def obter_hora_atual_string():
     tempo_local = time.localtime()
     hora = tempo_local.tm_hour
@@ -8,11 +7,7 @@ def obter_hora_atual_string():
     return f"{hora:02d}:{minuto:02d}"
 
 
-print(obter_hora_atual_string()) 
- print("buzer bip bip bip")
-print("alarme configurado para"+config_alarme)
-
 while True:
     if state == 0:
         print(obter_hora_atual_string())              
-time.sleep(360)   
+    time.sleep(60.0)
