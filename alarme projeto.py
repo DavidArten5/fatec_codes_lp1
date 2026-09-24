201d48a0535f2607e8cdb0b642d3817c1066f1e9
DavidArten5 <davidarten5@gmail.com>
2026-09-24 17:06:00 -0300

    o alarme funciona
    agora a funçao de alarme funciona  mas penso em mudar

--- alarme projeto.py
+++ alarme projeto.py
@@ -6,8 +6,13 @@ def obter_hora_atual_string():
     minuto = tempo_local.tm_min
     return f"{hora:02d}:{minuto:02d}"
 
+alarme_config = input("digite o horario ")
 
 while True:
     if state == 0:
-        print(obter_hora_atual_string())              
+        print(obter_hora_atual_string()) 
+        if alarme_config == obter_hora_atual_string():
+            print("beeeeeeeeeep beeeeeeeep")
+
     time.sleep(60.0)
+
